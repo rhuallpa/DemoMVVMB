c00fc474680e17176ff7dde9fdc517d48cad5713
Usuario <Usuario@DESKTOP-5FGS8HH>
2023-12-03 17:24:06 -0500

    Agregar archivos de proyecto.

---
+++ DemoMVVMB/DemoMVVMB/Models/TaskModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoMVVMB.Models
+{
+    public class TaskModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool Status { get; set; }
+        public TaskModel(string title, bool status, string description)
+        {
+            Title = title;
+            Status = status;
+            Description = description;
+        }
+    }
+}
